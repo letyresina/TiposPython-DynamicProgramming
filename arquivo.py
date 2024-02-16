990c68e774284f1c85d4a084402fb02d692b90bf
letyresina <leticiaresina@gmail.com>
2024-02-16 09:35:36 -0300

    mais alguns detalhes sobre tuplas

--- arquivo.py
+++ arquivo.py
@@ -22,6 +22,42 @@ print(tuplaString[3])
 tuplaDiversos = tuple(['for', [1,2], True])
 print(tuplaDiversos)
 print(tuplaDiversos[2])
+
 # só daria para inserir CASO e APENAS dentro da tupla houver algo mutável, como uma lista, em que:
 tuplaDiversos[1].append(4)
 print(tuplaDiversos)
+
+# Outra forma de modificar seria transformar a tupla em lista e depois alterar...
+
+# Então, uma forma 
+# Reforçando novamente, a tupla é imutável, mas você pode ter vários valores diferentes nela como
+tuplaDiversos2 = tuple(['for', [1,2], True])
+print(tuplaDiversos2)
+listaDiversa = list(tuplaDiversos2)
+posicaoInsercao = listaDiversa.index('for')+1 # pego um index de referência para meu ponto de inserção
+listaDiversa.insert(posicaoInsercao, 'hoje') # insiro depois da posição desejada que defini anteriormente
+novaTuplaDiversa = tuple(listaDiversa)
+print(novaTuplaDiversa)
+
+# Concatenando tuplas
+tuplaConcatenada = tuplaDiversos + tuplaString
+print(tuplaConcatenada)
+
+# Descompactando tuplas e nomeando variáveis de cada uma
+tuplaDescompactando = (4, 5, 6)
+a, b, c = tuplaDescompactando # a = 4; b = 5; c = 6
+print(a) # posso chamar então pela variável
+
+# exemplo com tupla de tupla
+tuplaDescompactando2 = (2, 4), (6, 8)
+print(tuplaDescompactando2)
+(a, b), (c, d) = tuplaDescompactando2
+print(c)
+
+# Descompactação de variáveis e iterar sobre sequência de tplas ou listar
+seq = [(1, 2, 3), (4, 5, 6), (7, 8, 9)]
+
+# Nessa parte, ele vai pegar cada indice de cada tupla dentro da lista e chamar pela variável
+for a, b, c in seq:
+    print('a={0}; b={1}, c={2}'.format(a, b, c))
+# Ou seja, o a da primeira tupla é 1, o a da segunda tupla é 4, o a da terceira tupla é 7 e assim sucessivamente
